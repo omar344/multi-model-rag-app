4069eb6ce8004f7d5ac8907b1df84bbdad8b134a
omar344 <3marabadi@gmail.com>
2025-03-26 15:02:14 +0200

    Return file_id from upload_data function

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -18,7 +18,7 @@ class DataController(BaseController):
         return True, ResponseSignal.FILE_UPLOAD_SUCCESS.value
     
     
-    def generate_unique_filename(self, orig_file_name: str, project_id: str):
+    def generate_unique_filepath(self, orig_file_name: str, project_id: str):
 
         random_key = self.generate_random_string()
         project_path = ProjectController().get_project_path(project_id = project_id)
@@ -37,7 +37,7 @@ class DataController(BaseController):
                 project_path,
                 f"{random_key}_{cleaned_file_name}"
             )
-        return new_file_path
+        return new_file_path,  f"{random_key}_{cleaned_file_name}"
     
 
     def get_clean_file_name(self, orig_file_name: str):
